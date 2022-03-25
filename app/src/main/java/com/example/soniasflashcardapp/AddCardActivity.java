0c0c4af741753b51545bfad29d8c77bdfeef9f61
Sonia <soniabendre02@gmail.com>
2022-03-25 15:07:15 -0400

    Add functionality to create custom flashcard

--- app/src/main/java/com/example/soniasflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/soniasflashcardapp/AddCardActivity.java
@@ -14,6 +14,7 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+
         findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +25,7 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("here");
                 Intent data = new Intent(); // create a new Intent, this is where we will put our data
                 data.putExtra("question", ((EditText) findViewById(R.id.question)).getText().toString()); // puts one string into the Intent, with the key as 'string1'
                 data.putExtra("answer", ((EditText) findViewById(R.id.answer)).getText().toString()); // puts another string into the Intent, with the key as 'string2
--- app/src/main/java/com/example/soniasflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/soniasflashcardapp/MainActivity.java
@@ -7,7 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+        TextView questionSideView = findViewById(R.id.question_side_view);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        flashcardDatabase.initFirstCard();
+        allFlashcards = flashcardDatabase.getAllCards();
 
+        // sets initial question to be displayed
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            flashcardQuestion.setText(allFlashcards.get(0).getQuestion());
+            flashcardAnswer.setText(allFlashcards.get(0).getAnswer());
+        }
+
+        // makes answer show when question clicked
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // makes question show when answer clicked
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // handles adding button
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,10 +65,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // handles next button
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (allFlashcards.size() == 0) {
+                    return;
+                }
+
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(questionSideView,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                flashcardAnswer.callOnClick();
+                flashcardQuestion.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                flashcardAnswer.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+            }
+
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
         super.onActivityResult(requestCode, resultCode, data);
@@ -46,6 +105,9 @@ public class MainActivity extends AppCompatActivity {
             String answer = data.getExtras().getString("answer");
             flashcardQuestion.setText(question);
             flashcardAnswer.setText(answer);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
